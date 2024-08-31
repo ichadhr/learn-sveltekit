8221581a0bc7ad6ed5094518521f6e975edc0285
ichadhr <ichadhr@gmail.com>
2024-09-01 00:53:40 +0700

    add z-index to navigation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -88,9 +88,9 @@
 	<title>{title}</title>
 </svelte:head>
 
-<header class="top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+<header class="sticky top-0 flex h-16 items-center gap-4 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 md:px-6 z-10">
 	<nav
-		class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
+		class="sticky hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
 	>
 		<a href="/" class="flex items-center gap-2 text-lg font-semibold md:text-base">
 			<Package2 class="h-6 w-6" />
